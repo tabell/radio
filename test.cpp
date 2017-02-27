75e5e211f6900ee6ff0b0b39fd698ea443902f75
Alex Bell <t.alexander.bell@gmail.com>
2017-02-27 12:36:50 -0800

    connect src to adder
    also add file sink

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <gnuradio/analog/sig_source_f.h>
 #include <gnuradio/audio/sink.h>
 #include <gnuradio/blocks/add_ff.h>
+#include <gnuradio/blocks/file_sink.h>
 
 #include <iostream>
 
@@ -9,6 +10,8 @@ using std::cout;
 using gr::top_block;
 using gr::analog::sig_source_f;
 using gr::blocks::add_ff;
+using gr::blocks::file_sink;
+using gr::flowgraph;
 
 int main(int argc, char const *argv[])
 {
@@ -24,6 +27,13 @@ int main(int argc, char const *argv[])
 		= sig_source_f::make(32000, gr::analog::GR_SIN_WAVE, 440, 0.5, 0);
 
 	add_ff::sptr add1 = add_ff::make();
+
+	tb->connect(src1,0,add1,0);
+	tb->connect(src2,0,add1,1);
+
+	file_sink::sptr file_out = file_sink::make(4,"/dev/stdout");
+
+	tb->connect(add1,0,file_out,0);
 
 	tb->run();
 	return 0;
