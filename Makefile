aa356b35bdeded6067a74f33ced1ab37d88d38e2
Your Name <you@example.com>
2017-02-27 15:08:02 -0800

    added qt stuff, not working yet

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 CC = g++
-LIBS = -lboost_system -lgnuradio-runtime -lgnuradio-analog -lgnuradio-blocks
+HEADERS = -I/usr/include/qt5/QtWidgets -I/usr/include/qt5
+QT_LIBS = -lQtGui
+GNURADIO_LIBS = -lboost_system -lgnuradio-runtime -lgnuradio-analog -lgnuradio-blocks -lgnuradio-qtgui
+FLAGS = -fPIE
 test: test.cpp
-	$(CC) test.cpp $(LIBS) -o test
+	$(CC) test.cpp $(HEADERS) $(GNURADIO_LIBS) $(QT_LIBS) $(FLAGS) -o test
--- test.cpp
+++ test.cpp
@@ -3,6 +3,10 @@
 #include <gnuradio/audio/sink.h>
 #include <gnuradio/blocks/add_ff.h>
 #include <gnuradio/blocks/file_sink.h>
+#include <gnuradio/qtgui/time_sink_f.h>
+
+#include <QApplication>
+#include <QtCore>
 
 #include <iostream>
 
@@ -12,29 +16,45 @@ using gr::analog::sig_source_f;
 using gr::blocks::add_ff;
 using gr::blocks::file_sink;
 using gr::flowgraph;
+using gr::qtgui::time_sink_f;
 
-int main(int argc, char const *argv[])
+const double samplerate = 32;
+
+gr::top_block_sptr tb;
+void done() {
+    tb->stop();
+    tb->wait();
+}
+int main(int argc, char *argv[])
 {
 	cout << "Hello world" << std::endl;
 	// top block
-	gr::top_block_sptr tb = gr::make_top_block("Test flowgraph");
+	tb = gr::make_top_block("Test flowgraph");
 
 	// sine wave 350 hz
 	sig_source_f::sptr src1
-		= sig_source_f::make(32000, gr::analog::GR_SIN_WAVE, 350, 0.5, 0);
+		= sig_source_f::make(samplerate, gr::analog::GR_SIN_WAVE, 350, 0.5, 0);
 	// sine wave 440 hz
 	sig_source_f::sptr src2
-		= sig_source_f::make(32000, gr::analog::GR_SIN_WAVE, 440, 0.5, 0);
+		= sig_source_f::make(samplerate, gr::analog::GR_SIN_WAVE, 440, 0.5, 0);
 
 	add_ff::sptr add1 = add_ff::make();
 
 	tb->connect(src1,0,add1,0);
-	tb->connect(src2,0,add1,1);
+//	tb->connect(src2,0,add1,1);
 
-	file_sink::sptr file_out = file_sink::make(4,"/dev/stdout");
+	time_sink_f::sptr qt_time = time_sink_f::make(512,samplerate,"Kitchen sinc");
 
-	tb->connect(add1,0,file_out,0);
+	tb->connect(add1,0,qt_time,0);
 
-	tb->run();
-	return 0;
+//    qt_time->exec();
+
+    QApplication a(argc,argv);
+
+    a.connect(a,aboutToQuit,done);
+//    cout << "Calling top_block::run" << std::endl;
+//	tb->run();
+    cout << "Calling QApp::exec" << std::endl;
+//    qt_time->show();
+	return a.exec();
 }
